9fdd40715c713dae9129ad28f534ba1c11a00c4d
towsifzilani <towsifzilani@gmail.com>
2022-08-04 05:10:30 +0600

    Reaction Timer setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
-  <button @click="start">Play</button>
+  <button class="playButton" @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" />
+
 </template>
 
 <script>
+import Block from './components/Blocks.vue'
 
 export default {
   name: 'App',
-  components: {},
+  components: { Block },
   data() {
     return {
       isPlaying: false,
@@ -18,13 +21,13 @@ export default {
     start() {
       this.isPlaying = true
       this.delay = 2000 + Math.random() * 5000
-      console.log(this.delay)
     }
   }
 }
 </script>
 
 <style>
+body { background: url('./assets/bg.jpg') no-repeat center center}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -33,4 +36,17 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+
+.playButton {
+  padding:10px 20px;
+  cursor: pointer;
+  border:none;
+}
+.playButton:hover { 
+  background: #6777ef;
+  color: white;
+  border:none;
+}
+
+h1 { color:white}
 </style>
--- src/components/Blocks.vue
+++ src/components/Blocks.vue
@@ -1,13 +1,35 @@
 <template>
-  
+  <div class="block" v-if="showBlock">
+    Click Me
+  </div>
 </template>
 
 <script>
 export default {
+    props: [ 'delay' ],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock=true
+        }, this.delay);
 
+    }
 }
 </script>
 
 <style>
-
+    .block {
+    width: 300px;
+    border-radius: 10px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+    font-size: 30px;
+  }
 </style>
